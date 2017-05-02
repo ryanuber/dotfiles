2003bee964e07c04e2e1ad725f4bb3adc33d5aec
Ryan Uber <ru@ryanuber.com>
2017-05-02 00:07:19 -0700

    Adding neovim

--- os/units/neovim.sh
+++ os/units/neovim.sh
@@ -3,9 +3,6 @@ set -e
 
 case $OS in
 ubuntu)
-    sudo apt-get install -y software-properties-common
-    sudo add-apt-repository ppa:neovim-ppa/stable
-    sudo apt-get update
     sudo apt-get install -y neovim
     ;;
 
