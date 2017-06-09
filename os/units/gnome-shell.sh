8bc00a3b6d405d40973bf3a777d17e0141bdbda9
Ryan Uber <ru@ryanuber.com>
2017-06-09 13:25:48 -0700

    Adding extensions

--- os/units/gnome-shell.sh
+++ os/units/gnome-shell.sh
@@ -6,7 +6,11 @@ gsettings set org.gnome.desktop.interface enable-animations false
 
 # Why topicons isn't built into the Gnome shell, I know not.
 sudo dnf install -y gnome-shell-extension-topicons-plus
-gnome-shell-extension-tool -e TopIcons@phocean.net
+gnome-shell-extension-tool -e TopIcons@phocean.net || :
+
+# Disable top hot corner. Also not sure why this is not a built-in option.
+sudo dnf install -y gnome-shell-extension-no-topleft-hot-corner
+gnome-shell-extension-tool -e nohotcorner@azuri.free.fr || :
 
 # Configure mouse settings
 gsettings set org.gnome.desktop.peripherals.mouse natural-scroll false
@@ -21,3 +25,16 @@ gsettings set $SCHEMA prev-tab '<Primary>Left'
 gsettings set $SCHEMA next-tab '<Primary>Right'
 gsettings set $SCHEMA new-tab '<Primary>t'
 gsettings set org.gnome.Terminal.Legacy.Settings default-show-menubar false
+
+# Configure the gnome weather shell extension
+sudo dnf install -y gnome-shell-extension-openweather
+SCHEMA=org.gnome.shell.extensions.openweather
+gsettings set $SCHEMA unit fahrenheit
+gsettings set $SCHEMA wind-speed-unit mph
+gsettings set $SCHEMA pressure-unit psi
+gsettings set $SCHEMA refresh-interval-current 600
+gsettings set $SCHEMA refresh-interval-forecast 3600
+gsettings set $SCHEMA appid-fc 2191ed80fb23838ebede43bc19580b7f
+gsettings set $SCHEMA weather-provider darksky.net
+gsettings set $SCHEMA city '33.1968352521268,-117.285215120784>Oceanside, San Diego County, California, 92056, United States of America >-1'
+gnome-shell-extension-tool -e openweather-extension@jenslody.de || :
