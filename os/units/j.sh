569ffef55849813491f1545adde2efe36df87d94
Ryan Uber <ru@ryanuber.com>
2018-01-10 23:31:51 -0800

    better support for j

--- os/units/j.sh
+++ os/units/j.sh
@@ -10,9 +10,11 @@ mac)
     ;;
 esac
 
+cp ../bin/j $HOME/bin/j
 $SED -i /j-start/,/j-end/d $PROFILE
 cat >> $PROFILE <<EOF
 # j-start
 export J_SEARCHDIRS="\$HOME/git:\$GOPATH/src/*/*"
+. $HOME/bin/j
 # j-end
 EOF
