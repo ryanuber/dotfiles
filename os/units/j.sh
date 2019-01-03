64656923210b8a662fb3c6519fa24f36c820320d
Ryan Uber <ru@ryanuber.com>
2019-01-03 09:04:46 -0800

    Update golang, swap to ~/code

--- os/units/j.sh
+++ os/units/j.sh
@@ -14,7 +14,7 @@ cp ../bin/j $HOME/bin/j
 $SED -i /j-start/,/j-end/d $PROFILE
 cat >> $PROFILE <<EOF
 # j-start
-export J_SEARCHDIRS="\$HOME/git:\$GOPATH/src/*/*"
+export J_SEARCHDIRS="\$HOME/code"
 . $HOME/bin/j
 # j-end
 EOF
